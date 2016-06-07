f164714ef7cf34848f769de9cace81797bef3e87
Anouar BOUGDIRA <a.bougdira@integration-2-pornic.admin.alphalink.fr>
2016-06-07 12:39:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3868c4202c3b4f1ff5bad9c025fbb93d743c8467787e7913fec2d3e3cc9bf3791fad58b56e367cad7f8984b76eaf196e36e777043c186e5509c3004af5196fc5'
